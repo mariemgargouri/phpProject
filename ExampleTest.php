8e70f0fab995f1063ae1cc6a71d5b59cc1733f40
mariemgargouri <mariem.grc@gmail.com>
2016-04-25 22:41:09 +0200

    first commit

--- ExampleTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-public function testBasicExample()
-{
-	$data = [10, 20, 30];
-	$result = array_sum($data);
-	$this->assertEquals(60, $result);//permet de comparer deux valeurs, ici 60 et $result
-}
